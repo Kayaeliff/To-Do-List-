077b958e017ab62651e5a74890dfa1400e583855
“Elif <elifffkayaa@gmail.com>
2024-06-20 19:53:07 +0300

    To do list

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './companents/Todo'
+
+const App = () => {
+  return (
+    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 red py-4 min-h-screen'>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
